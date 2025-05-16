355246e82b81fdee5c8debf4a62f80c50fc0ef91
Андрей <andrey.shkuropatov@mail.ru>
2025-05-16 09:05:27 +0700

    книги

---
+++ lab6/book.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: "Джоан Роулинг",
+        name: "Гарри Поттер и философский камень",
+        publicationYear: 1997,
+        price: 15.99,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Джордж Оруэлл",
+        name: "1984",
+        publicationYear: 1949,
+        price: 12.50,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: "Фрэнсис Скотт Фицджеральд",
+        name: "Великий Гэтсби",
+        publicationYear: 1925,
+        price: 10.75,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Лев Толстой",
+        name: "Анна Каренина",
+        publicationYear: 1877,
+        price: 14.25,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: "Фёдор Достоевский",
+        name: "Преступление и наказание",
+        publicationYear: 1866,
+        price: 11.99,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Эрнест Хемингуэй",
+        name: "Старик и море",
+        publicationYear: 1952,
+        price: 9.99,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: "Джейн Остин",
+        name: "Гордость и предубеждение",
+        publicationYear: 1813,
+        price: 8.50,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: "Габриэль Гарсиа Маркес",
+        name: "Сто лет одиночества",
+        publicationYear: 1967,
+        price: 13.25,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Антуан де Сент-Экзюпери",
+        name: "Маленький принц",
+        publicationYear: 1943,
+        price: 7.99,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: "Джером Д. Сэлинджер",
+        name: "Над пропастью во ржи",
+        publicationYear: 1951,
+        price: 10.25,
+        bestseller: true,
+        inStock: false
+    }
+];
